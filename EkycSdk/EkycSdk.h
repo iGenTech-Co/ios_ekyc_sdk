ca480d0f2b0c9b2f7b4d51052356f40071d42b27
Zeyad Tarek <Ziyad@igentech.co>
2025-12-10 23:16:24 +0200

    Initial Commit

---
+++ EkycSdk/EkycSdk.h
@@ -0,0 +1,18 @@
+//
+//  EkycSdk.h
+//  EkycSdk
+//
+//  Created by Zeyad on 12/10/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EkycSdk.
+FOUNDATION_EXPORT double EkycSdkVersionNumber;
+
+//! Project version string for EkycSdk.
+FOUNDATION_EXPORT const unsigned char EkycSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EkycSdk/PublicHeader.h>
+
+
