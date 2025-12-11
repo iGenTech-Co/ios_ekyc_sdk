1c93c4459246354385895b521ffc2aa68feecc04
Zeyad Tarek <Ziyad@igentech.co>
2025-12-11 14:04:23 +0200

    Update eKYC base URL

--- Sources/EkycSdk/Core/EkycManager.swift
+++ Sources/EkycSdk/Core/EkycManager.swift
@@ -4,8 +4,9 @@ public class EkycManager {
     
     public static let shared = EkycManager()
     
-    private let baseURL = "https://www.youtube.com" 
-    
+ 
+    private let baseURL = "https://verify.bantech.ae/?vid=%2Fapi%2F9f998bfb185f4556bed6889ebda98cd4%2Fajil-pay%2Fa09127b249fd4338bc645aba87474477%2Fweb%3Fexpires%3D1765456569%26signature%3Df99b504dd820291419b2a0b1a41001b1ade224400b82f23c870454b4c061acf0"
+ 
     private init() {}
     
     public weak var delegate: EkycDelegate?
